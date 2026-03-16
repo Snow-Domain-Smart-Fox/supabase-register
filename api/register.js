9455a47c0b9f71b7a51c92614eb62d8a5e9effd2
杨人瑞 <yangrenruiyrr@163.com>
2026-03-16 18:35:36 +0800

    Update register.js

--- api/register.js
+++ api/register.js
@@ -93,10 +93,8 @@ module.exports = async (req, res) => {
       password: randomPassword,
       email_confirm: true, // 自动确认邮箱
       send_email_confirm: false, // 不发送确认邮件
-      options: {
-          data: {
-              luogu_uid: luoguuid,
-          }
+      user_metadata: {
+          luogu_uid: luoguuid,
       }
     });
 
